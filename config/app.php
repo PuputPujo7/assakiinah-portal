06b557bc628238b958200b0d692fc2aa6c55e0f0
PuputPujo7 <puputpujo7@gmail.com>
2022-11-12 11:21:01 +0700

    update lagi config/app.php

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://asskiinahstore.com'),
+    'url' => env('APP_URL', 'https://asskiinahstore.com'),
 
     'asset_url' => env('ASSET_URL'),
 
